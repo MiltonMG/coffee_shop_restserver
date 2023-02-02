e93dd21bd64034ce850e1eb5a20a1300dccde4c8
MiltonMG <milton.elsalvador26@gmail.com>
2023-02-02 12:01:51 -0600

    Sign in and sign out with google

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const Usuario = require('../models/usuario');
 
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -55,7 +56,64 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req = request, res = response) => {
+
+    const {id_token} = req.body;
+    
+
+    try {
+        
+        const { name, img, email } = await googleVerify(id_token);
+
+        //Generar referencia para ver si correo existe en la base de datos
+        let usuario = await Usuario.findOne({email});
+
+        //Si el usuario no existe lo crearemos
+        if (!usuario) {
+            
+            const data = {
+                name,
+                email,
+                img,
+                password: ':P', //el password no importa por q nos estamos autenticando con correo de google
+                role: "USER_ROLE",
+                google: true
+            }
+
+            usuario = new Usuario( data );
+
+            await usuario.save();
+
+        }
+
+        //Si el usuario en BD tiene el estado en false, negare su acceso en la aplicacion
+        if (!usuario.status) {
+            return res.status(401).json({
+                msg: "El status es falso"
+            })
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+        
+    } catch (error) {
+
+        res.status(400).json({
+            error: error,
+            msg: "El token de google no se pudo verificar"
+        })
+    }
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
